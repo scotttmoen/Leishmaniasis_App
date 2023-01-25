3ca98016e8bb56f7b266f629dbb654f85a588c7a
stmoen <117862405+stmoen@users.noreply.github.com>
2023-01-25 17:06:58 -0600

    When "SUBMIT", radio type is noted. Next add crosshair drawable and crosshair imageview.

--- app/src/main/java/com/example/diagnosticreadingapp/Activity2.kt
+++ app/src/main/java/com/example/diagnosticreadingapp/Activity2.kt
@@ -3,11 +3,13 @@ package com.example.diagnosticreadingapp
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class Activity2 : AppCompatActivity() {
     private lateinit var rGroup: RadioGroup
@@ -21,21 +23,42 @@ class Activity2 : AppCompatActivity() {
         val submitAnalyzeButton = findViewById<Button>(R.id.submitAnalyze)
         val imageAnalyze = this.intent.data
 
+        //Before this is basic, this will be added to imageview that is added to the layout
+        var crossHairIcon = ContextCompat.getDrawable(
+            applicationContext,
+            R.drawable.ic_launcher_background  //<-- name of xml file in res folder
+        )
+
+
         rGroup = findViewById(R.id.rGroup)
         Log.d("guitar imageUri", imageAnalyze.toString())
-
-        //rGroup.checkedRadioButtonId
-
         imageAnalyzeView.setImageURI(imageAnalyze)
 
          //This registers change to radio button
-        rGroup.setOnCheckedChangeListener { rGroup, _ ->
-            Log.d("guitar changed radio", rGroup.toString())
 
+        imageAnalyzeView.setOnTouchListener { v, event ->
+            when (event.action){
+                MotionEvent.ACTION_MOVE -> {
+                    v.x = event.x
+                    v.y = event.y
+                    Log.d("guitar location x", v.x.toString())
+                    Log.d("guitar location y", v.y.toString())
+                }
+            }
+            true
         }
         submitAnalyzeButton.setOnClickListener {
-            Log.d("guitar radio submit", rGroup.toString())
-            //whichRadio()
+            var radioType = ""
+            if  (rGroup.checkedRadioButtonId.toString() == R.id.patientSamples.toString()){
+                radioType = "patient"
+            }else if (rGroup.checkedRadioButtonId.toString() == R.id.negativeControls.toString()){
+                radioType = "negative"
+            }else if (rGroup.checkedRadioButtonId.toString() == R.id.positiveControls.toString()){
+                radioType = "positive"
+            }else{
+                radioType = "none"
+            }
+            Log.d("guitar submit",radioType)
 
         }
     }
@@ -44,11 +67,6 @@ class Activity2 : AppCompatActivity() {
     fun whichRadio(view: View) {
 
         val individualRadio = rGroup.checkedRadioButtonId
-        Log.d("guitar", "General press1.")
-        ///THis line
-        //val selectedRadio = individualRadio.let { rGroup.findViewById<RadioGroup>(it) }
-        Log.d("guitar", "General press2.")
-        //val selectedString = selectedRadio?.checkedRadioButtonId
         val selectedString = individualRadio
         Log.d("guitar which radio", individualRadio.toString())
     }
